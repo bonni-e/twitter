c2880c48c1e05cb5d62e6c64537e2981410ce918
Kim Jiyeon <KimJiyeon@Kimui-MacBook-Pro.local>
2024-02-22 20:44:33 +0900

    Initialize Firebase SDK

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBUCUpwyL7LFFAfHrn86XU1eYJoG9kWL1k",
+    authDomain: "twitter-2980e.firebaseapp.com",
+    projectId: "twitter-2980e",
+    storageBucket: "twitter-2980e.appspot.com",
+    messagingSenderId: "155621189492",
+    appId: "1:155621189492:web:606d9a4cbc70488bf26e21",
+    measurementId: "G-CG534ME7VY"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
