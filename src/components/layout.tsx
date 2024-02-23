4863c02380881c49aa9c456f1bd6c6ae34f3e3d0
Kim Jiyeon <KimJiyeon@Kimui-MacBook-Pro.local>
2024-02-23 17:12:35 +0900

    Add Logout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,23 @@
-import { Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
+import styled from "styled-components";
+import { auth } from "../firebase";
+
+const Navi = styled.nav`
+`;
+
+const Logout = styled.button`
+`;
 
 export default function Layout() {
+    const onClick = (e : React.MouseEvent<HTMLButtonElement>) => {
+        auth.signOut();
+    }
+
     return (
         <>
-            <address>layout</address>
+            <Navi>
+                <Logout onClick={onClick}>Logout</Logout>
+            </Navi>
             <Outlet />
             {/* Renders the child route's element, if there is one. */}
         </>
